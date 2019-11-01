cfc9ea57c20f4882379685f58aa5422cbbcfd240
Anton Jacobsson <ajacobsson98@gmail.com>
2019-11-01 17:25:25 +0100

    feat(cordova): onBackbutton event

--- src/cordova-events.ts
+++ src/cordova-events.ts
@@ -1,6 +1,10 @@
 
 export class CordovaEvents {
 
+    constructor() {
+        document.addEventListener("backbutton", this.onBackKeyDown, false);
+    }
+
     public waitForDeviceReady(): Promise<boolean> {
         let p = new Promise<boolean>((resolve, reject) => {
             document.addEventListener('deviceready', () => {
@@ -8,6 +12,10 @@ export class CordovaEvents {
             }, false);
         });
         return p;
+    }
+
+    public onBackKeyDown() {
+        alert("BACKBUTTON");
     }
 
 }
